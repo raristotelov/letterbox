ac326a3624baf4f0757c35d34c54421abf5efe97
Radostin Aristotelov <radostin.aristotelov@gmail.com>
2022-06-04 18:53:35 +0300

    Removed console.logs

--- server/smtpServer/server.js
+++ server/smtpServer/server.js
@@ -8,7 +8,6 @@ const { newsService } = require('../src/services');
 const server = new SMTPServer({
     onData(stream, session, callback) {
         simpleParser(stream, {}, (err, parsed) => {
-            console.log("email")
             if (err) {
                 console.log(err);
             }
@@ -28,8 +27,6 @@ const server = new SMTPServer({
                 image: letter.image ? letter.image : 'https://www.andromo.com/blog/wp-content/uploads/2020/12/news-1.jpg',
             };
 
-            console.log("New email hereeee");
-
             try {
                 newsService.add(letterData, letter.newsletter);
             } catch (error) {
@@ -37,7 +34,6 @@ const server = new SMTPServer({
             }
         });
         stream.on('end', () => {
-            conole.log("Ended");
             let err;
             if (stream.sizeExceeded) {
                 err = new Error('Error: message exceeds fixed maximum message size 10 MB');
