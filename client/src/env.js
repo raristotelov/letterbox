0ba305523dcd082253d2313ad99cd8924a80197f
Radostin Aristotelov <radostin.aristotelov@gmail.com>
2022-09-11 11:42:13 +0300

    Changed some variables for testing purposes

--- client/src/env.js
+++ client/src/env.js
@@ -7,7 +7,7 @@ export const SERVER_ADDRESS = serverAddress[process.env.NODE_ENV]
 
 const domainAddress = {
     development: 'http://localhost:3000',
-    production: 'http://letterbox.tk/'
+    production: 'http://letterbox.tk'
 }
 
 export const DOMAIN_ADDRESS = domainAddress[process.env.NODE_ENV];
